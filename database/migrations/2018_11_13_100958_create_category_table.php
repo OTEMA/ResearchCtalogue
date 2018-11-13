f30f7c217f541e1643bb8d5a8f28a0e4dc6b8f93
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-13 13:14:41 +0300

    Added columns to the category migration file

--- database/migrations/2018_11_13_100958_create_category_table.php
+++ database/migrations/2018_11_13_100958_create_category_table.php
@@ -4,18 +4,19 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTable extends Migration
-{
+class CreateCategoryTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
-            
+            $table->integer('parent_id')->default('0');
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -25,8 +26,8 @@ class CreateCategoryTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('category');
     }
+
 }
