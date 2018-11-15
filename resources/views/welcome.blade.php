a425eaed8ab3518f867c05bca3e11d0fbc5bdf6a
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-15 12:05:00 +0300

    Added publications to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,96 +3,99 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/backend_images/favicon.png')}}">
+        <title>MFarmer Dashboard</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{asset('css/backend_css/bootstrap.min.css')}}" />
+        <link rel="stylesheet" href="{{asset('css/backend_css/bootstrap-responsive.min.css')}}" />
+        <link rel="stylesheet" href="{{asset('css/backend_css/matrix-style.css')}}" />
+        <link rel="stylesheet" href="{{asset('css/backend_css/select2.css')}}" />
+        <link rel="stylesheet" href="{{asset('css/backend_css/matrix-media.css')}}" />
+        <link rel="stylesheet" href="{{asset('fonts/backend_fonts/css/font-awesome.css')}}" />
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
             .links > a {
-                color: #636b6f;
+                color: #0000F0;
                 padding: 0 25px;
+                margin: 0 0 25px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
             @endif
+            @endauth
+        </div>
+        @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <div class="content">
+            <div class="widget-content nopadding">
+                <table class="table table-bordered data-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>CATEGORY</th>
+                            <th>PUBLICATION CODE</th>
+                            <th>AUTHOR</th>
+                            <th>EMAIL</th>
+                            <th>DESCRIPTION</th>
+                            <th>IMAGE</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($publications as $publication)
+                        <tr class="gradeX">
+                            <td class="center">{{ $publication->id }}</td>
+                            <td>{{ $publication->category_id }}</td>
+                            <td class="center">{{ $publication->author_name }}</td>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                            <td class="center">{{ $publication->author_email }}</td>
+                            <td>{{ $publication->path }}</td>
+                            <td><a href="#" class="btn btn-success btn-mini">View <i class="icon icon-eye-open"></i></a> |</td> 
+                            <td><a href="{{url('admin/edit_category/'.$publication->id)}}" class="btn btn-warning btn-mini">Edit <i class="icon icon-edit"></i></a> | </td>
+                            <td><a rel="{{$publication->id}}" rel1="delete_category" href="javascript:" class="btn btn-danger btn-mini deleteCategory">Delete <i class="icon icon-trash"></i></a></td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
+        <script src="{{asset('js/backend_js/excanvas.min.js')}}"></script>
+        <!--Jquery-->
+        <script src="{{asset('js/backend_js/jquery.min.js')}}"></script> 
+        <script src="{{asset('js/backend_js/jquery.ui.custom.js')}}"></script> 
+        <script src="{{asset('js/backend_js/bootstrap.min.js')}}"></script> 
+        <script src="{{asset('js/backend_js/matrix.js')}}"></script> 
+        <script src="{{asset('js/backend_js/fullcalendar.min.js')}}"></script>
+        <script src="{{asset('js/backend_js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('js/backend_js/matrix.tables.js')}}"></script>
+        <script src="{{asset('js/backend_js/matrix.interface.js')}}"></script> 
+        <script src="{{asset('js/backend_js/jquery.uniform.js')}}"></script>
+        <script src="{{asset('js/backend_js/select2.min.js')}}"></script> 
     </body>
 </html>
